6b2a5eb0ff793657d1c47ff7580963fad1df6012
Kaszanas <andrzej.bialecki94@gmail.com>
2023-08-19 02:19:43 +0200

    Added check_line doctest (#7)

--- src/parser/check_line.rs
+++ src/parser/check_line.rs
@@ -1,6 +1,19 @@
 use crate::parser::find_brackets::find_brackets;
 
 // REVIEW: This looks bad, is there a better way to do this?
+/// Checks a line in search for a specific command substring.
+///
+/// # Example
+///
+/// ```
+/// use texcollector::parser::check_line::check_line;
+///
+/// let line = "\\command{with_enclosed_content}".to_string();
+///
+/// if let Some(found_command_content) = check_line(line, "\\command") {
+///     println!("Got command content: {}", found_command_content);
+/// }
+/// ```
 pub fn check_line(line: String, command: &str) -> Option<String> {
     match line.contains(command) {
         true => {
