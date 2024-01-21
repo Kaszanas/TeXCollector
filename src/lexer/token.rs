554ab90bb9ca3877618a7954eec7cf76da7f9b2b
Kaszanas <andrzej.bialecki94@gmail.com>
2024-01-21 17:57:17 +0100

    Added Token definition, defining lexer

---
+++ src/lexer/token.rs
@@ -0,0 +1,48 @@
+use logos::{Lexer, Logos};
+use strum_macros::EnumDiscriminants;
+
+#[derive(Clone, Debug, Logos, PartialEq, Eq, EnumDiscriminants, clap::ValueEnum)]
+pub enum Token {
+    /// Left curly brace `'{'` character.
+    #[token("{")]
+    BraceOpen,
+
+    /// Right curly brace `'}'` character.
+    #[token("}")]
+    BraceClose,
+
+    /// Left bracket `'['` character.
+    #[token("]")]
+    BracketClose,
+
+    /// Right bracket `']'` character.
+    #[token("[")]
+    BracketOpen,
+
+    /// Input command, matching regex "\input".
+    /// This is a command that includes another file.
+    #[token(r"\input")]
+    InputCommand,
+
+    /// Use package command matching "\usepackage".
+    #[token(r"\usepackage")]
+    UsePackage,
+
+    /// Command for including a graphic in the final output file.
+    #[token(r"\includegraphics")]
+    IncludeGraphics,
+
+    /// A valid command name, including leading backslash `\`,
+    /// matching regex `r"\\[a-zA-Z]+"`.
+    #[regex(r"\\[a-zA-Z]+")]
+    CommandName,
+
+    /// Indicates an invalid command name, that should match everything
+    /// escaped sequence that has invalid syntax.
+    #[regex(r"\\[^a-zA-Z]")]
+    InvalidCommand,
+
+    /// A comment, matching anyway after a non-escaped percent-sign `'%'` is encountered.
+    #[regex("%.*")]
+    Comment,
+}
